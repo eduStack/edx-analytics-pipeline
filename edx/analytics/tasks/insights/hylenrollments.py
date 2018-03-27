d67564c6d19da7b362d67f3a6675327817e9b3b4
hyl <kuaile2911@126.com>
2018-03-27 16:24:49 +0800

    simplify code

--- edx/analytics/tasks/insights/hylenrollments.py
+++ edx/analytics/tasks/insights/hylenrollments.py
@@ -21,7 +21,7 @@ from edx.analytics.tasks.util.url import ExternalURL
 from edx.analytics.tasks.warehouse.load_internal_reporting_course_catalog import (
     LoadInternalReportingCourseCatalogMixin, CourseRecord
 )
-from edx.analytics.tasks.util.data import UniversalDataTask, LoadDataFromDatabaseTask
+from edx.analytics.tasks.util.data import UniversalDataTask, LoadDataFromDatabaseTask, LoadEventFromLocalFileTask
 
 log = logging.getLogger(__name__)
 DEACTIVATED = 'edx.course.enrollment.deactivated'
@@ -580,7 +580,86 @@ class AuthUserProfileSelectionTask(LoadDataFromDatabaseTask):
         return query
 
 
-class CourseEnrollmentEventsTask(EventLogSelectionMixin, luigi.Task):
+class CourseEnrollmentEventsTask(LoadEventFromLocalFileTask):
+    """
+    Task to extract enrollment events from eventlogs over a given interval.
+    This would produce a different output file for each day within the interval
+    containing that day's enrollment events only.
+    """
+    counter_category_name = 'Enrollment Events'
+
+    def get_event_row_from_line(self, line):
+        value = self.get_event_and_date_string(line)
+        if value is None:
+            return
+        event, date_string = value
+        self.incr_counter(self.counter_category_name, 'Inputs with Dates', 1)
+
+        event_type = event.get('event_type')
+        if event_type is None:
+            log.error("encountered event with no event_type: %s", event)
+            self.incr_counter(self.counter_category_name, 'Discard Missing Event Type', 1)
+            return
+
+        if event_type not in (DEACTIVATED, ACTIVATED, MODE_CHANGED):
+            self.incr_counter(self.counter_category_name, 'Discard Non-Enrollment Event Type', 1)
+            return
+
+        timestamp = eventlog.get_event_time_string(event)
+        if timestamp is None:
+            log.error("encountered event with bad timestamp: %s", event)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Timestamp', 1)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Something', 1)
+            return
+
+        event_data = eventlog.get_event_data(event)
+        if event_data is None:
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Event Data', 1)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Something', 1)
+            return
+
+        course_id = opaque_key_util.normalize_course_id(event_data.get('course_id'))
+        if course_id is None or not opaque_key_util.is_valid_course_id(course_id):
+            log.error("encountered explicit enrollment event with invalid course_id: %s", event)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing course_id', 1)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Something', 1)
+            return
+
+        user_id = event_data.get('user_id')
+        if user_id is None:
+            log.error("encountered explicit enrollment event with no user_id: %s", event)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing user_id', 1)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Something', 1)
+            return
+
+        mode = event_data.get('mode')
+        if mode is None:
+            log.error("encountered explicit enrollment event with no mode: %s", event)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing mode', 1)
+            self.incr_counter(self.counter_category_name, 'Discard Enroll Missing Something', 1)
+            return
+
+        self.incr_counter(self.counter_category_name, 'Output From Mapper', 1)
+        # reformat data for aggregation
+        return date_string, (course_id.encode('utf8'), user_id), timestamp, event_type, mode
+
+    def processing(self, data):
+        columns = ['date_string', 'course_id+user_id', 'timestamp', 'event_type', 'mode']
+        df = pd.DataFrame(data=data, columns=columns)
+
+        increment_counter = lambda counter_name: self.incr_counter(self.counter_category_name, counter_name,
+                                                                   1)
+
+        for (date_string, (course_id, user_id)), group in df.groupby(['date_string', 'course_id+user_id']):
+            values = group[['timestamp', 'event_type', 'mode']].get_values()
+
+            event_stream_processor = DaysEnrolledForEvents(course_id, user_id, self.interval, values,
+                                                           increment_counter)
+            for day_enrolled_record in event_stream_processor.days_enrolled():
+                yield day_enrolled_record
+
+
+class CourseEnrollmentEventsTask1(EventLogSelectionMixin, luigi.Task):
     """
     Task to extract enrollment events from eventlogs over a given interval.
     This would produce a different output file for each day within the interval
@@ -599,7 +678,7 @@ class CourseEnrollmentEventsTask(EventLogSelectionMixin, luigi.Task):
     _counter_dict = {}
 
     def __init__(self, *args, **kwargs):
-        super(CourseEnrollmentEventsTask, self).__init__(*args, **kwargs)
+        super(CourseEnrollmentEventsTask1, self).__init__(*args, **kwargs)
 
     def get_event_row_from_line(self, line):
         value = self.get_event_and_date_string(line)
@@ -737,12 +816,12 @@ class CourseEnrollmentEventsTask(EventLogSelectionMixin, luigi.Task):
 
     def run(self):
         self.init_local()
-        super(CourseEnrollmentEventsTask, self).run()
+        super(CourseEnrollmentEventsTask1, self).run()
         if not self.completed:
             self.completed = True
 
     def requires(self):
-        requires = super(CourseEnrollmentEventsTask, self).requires()
+        requires = super(CourseEnrollmentEventsTask1, self).requires()
         if isinstance(requires, luigi.Task):
             yield requires
 
--- edx/analytics/tasks/util/data.py
+++ edx/analytics/tasks/util/data.py
@@ -1,3 +1,5 @@
+import gzip
+
 import luigi
 import logging
 import traceback
@@ -5,7 +7,9 @@ import traceback
 from edx.analytics.tasks.common.mysql_load import get_mysql_query_results
 
 from edx.analytics.tasks.insights.database_imports import DatabaseImportMixin
+from edx.analytics.tasks.common.pathutil import EventLogSelectionDownstreamMixin, EventLogSelectionMixin
 from edx.analytics.tasks.util.url import ExternalURL
+from edx.analytics.tasks.util import eventlog, opaque_key_util
 
 log = logging.getLogger(__name__)
 
@@ -21,22 +25,31 @@ class UniversalDataTask(luigi.Task):
         return len(self.result) > 0 or self.completed
 
     def run(self):
+        self.init_env()
         data = self.load_data()
-        log.info('load {} data succ'.format(len(data)))
+        length = 0
+        if data:
+            length = len(data)
+        log.info('load {} data succ'.format(length))
         try:
             log.info('data processing......')
-            self.result = self.processing(data)
+            if length > 0:
+                self.result = self.processing(data)
+                log.info('{} data process completed'.format(length))
             self.completed = True
-            log.info('{} data process completed'.format(len(data)))
+            log.info('No data need to process')
             # self.output()
         except Exception, e:
-            log.error('Porcesing data error:{}'.format(traceback.format_exc()))
+            log.error('Processing data error:{}'.format(traceback.format_exc()))
 
     def processing(self, data):
         return data
 
     def load_data(self):
         return []
+
+    def init_env(self):
+        pass
 
 
 class LoadDataFromDatabaseTask(DatabaseImportMixin, UniversalDataTask):
@@ -54,3 +67,122 @@ class LoadDataFromDatabaseTask(DatabaseImportMixin, UniversalDataTask):
 
     def requires(self):
         yield ExternalURL(url=self.credentials)
+
+
+class LoadEventTask(EventLogSelectionMixin, UniversalDataTask):
+    batch_counter_default = 1
+
+    counter_category_name = 'Default Category'
+
+    _counter_dict = {}
+
+    def init_env(self):
+        super(LoadEventTask, self).init_env()
+        self.lower_bound_date_string = self.interval.date_a.strftime('%Y-%m-%d')  # pylint: disable=no-member
+        self.upper_bound_date_string = self.interval.date_b.strftime('%Y-%m-%d')  # pylint: disable=no-member
+
+    def _incr_counter(self, *args):
+        """ Increments a Hadoop counter
+
+        Note that this seems to be a bit slow, ~1 ms. Don't overuse this function by updating very frequently.
+        """
+        if len(args) == 2:
+            # backwards compatibility with existing hadoop jobs
+            group_name, count = args
+            # log.debug('reporter:counter:%s,%s' % (group_name, count))
+        else:
+            group, name, count = args
+            # log.debug('reporter:counter:%s,%s,%s' % (group, name, count))
+
+    def incr_counter(self, *args, **kwargs):
+        """ Increments a Hadoop counter
+
+        Since counters can be a bit slow to update, this batches the updates.
+        """
+        threshold = kwargs.get("threshold", self.batch_counter_default)
+        if len(args) == 2:
+            # backwards compatibility with existing hadoop jobs
+            group_name, count = args
+            key = (group_name,)
+        else:
+            group, name, count = args
+            key = (group, name)
+
+        ct = self._counter_dict.get(key, 0)
+        ct += count
+        if ct >= threshold:
+            new_arg = list(key) + [ct]
+            self._incr_counter(*new_arg)
+            ct = 0
+        self._counter_dict[key] = ct
+
+    def get_event_and_date_string(self, entity):
+        event = self.parse_event_from_entity(entity)
+        if event is None:
+            self.incr_counter('Event', 'Discard Unparseable Event', 1)
+            return None
+
+        event_time = self.get_event_time(event)
+        if not event_time:
+            self.incr_counter('Event', 'Discard Missing Time Field', 1)
+            return None
+
+        # Don't use strptime to parse the date, it is extremely slow
+        # to do so. Instead rely on alphanumeric comparisons.  The
+        # timestamp is ISO8601 formatted, so dates will look like
+        # %Y-%m-%d.  For example: 2014-05-20.
+        date_string = event_time.split("T")[0]
+
+        if date_string < self.lower_bound_date_string or date_string >= self.upper_bound_date_string:
+            # Slow: self.incr_counter('Event', 'Discard Outside Date Interval', 1)
+            return None
+
+        return event, date_string
+
+    def load_data(self):
+        return self.load_raw_events()
+
+    def parse_event_from_entity(self, line):
+        raise NotImplementedError
+
+    def load_raw_events(self):
+        raise NotImplementedError
+
+
+class LoadEventFromLocalFileTask(LoadEventTask):
+
+    def load_raw_events(self):
+        raw_events = []
+        for log_file in luigi.task.flatten(self.input()):
+            with log_file.open('r') as temp_file:
+                with gzip.GzipFile(fileobj=temp_file) as input_file:
+                    log.info('reading log file={}'.format(input_file))
+                    events = self.get_raw_events_from_log_file(input_file)
+                    if not events:
+                        continue
+                    raw_events.extend(events)
+        return raw_events
+
+    def get_raw_events_from_log_file(self, input_file):
+        raw_events = []
+        for line in input_file:
+            event_row = self.get_event_row_from_line(line)
+            if not event_row:
+                continue
+            raw_events.append(event_row)
+        return raw_events
+
+    def parse_event_from_entity(self, line):
+        return eventlog.parse_json_event(line)
+
+    def get_event_row_from_line(self, line):
+        return NotImplementedError
+
+
+class LoadEventFromMongo(LoadEventTask):
+
+    def parse_event_from_entity(self, document):
+        return document
+
+    def load_raw_events(self):
+        return []
